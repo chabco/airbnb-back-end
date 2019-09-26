3df58447b50f277d09f40e1b03e4c1fd5a5b1c5e
charlescortez <charlesmph@gmail.com>
2019-09-26 13:43:32 -0400

    Update with middleware

--- routes/host.js
+++ routes/host.js
@@ -1,11 +1,46 @@
 var express = require('express');
 var router = express.Router();
-const db = require('../db');
+var db = require('../db');
+
+const fs = require('fs');
 
 //    post('/host/homes')
-router.post('/homes',(req, res)=>{
+// middleware
+router.post('/homes', (req, res)=>{
     console.log(req.body);
-    res.json(req.body);
+
+    
+    
+    console.log(req.file);
+
+    if (!res.locals.logginIn) {
+        res.json ({
+            msg:"badToken"
+        })
+        return;
+    }
+
+
+    const { title,location, guests, pricePerNight, details, amenities, token } = req.body
+
+
+    const f = req.file;
+    const finalFilePath = f.destination+'/'+Date.now()+f.originalname;
+    const filePathForDb = finalFilePath.slice(8)
+    fs.rename(f.path, finalFilePath, (err) => {
+        if(err) {throw err}
+    })
+
+    const insertHomeQuery = `INSERT INTO homes (uid, title, location, guests, pricePerNight, details, amenities, imageUrl) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+
+    const dbValues = [res.locals.uid, title, location, guests, pricePerNight, details, amenities, filePathForDb]
+    db.query (insertHomeQuery, dbValues, (err) => {
+        if(err) throw err;
+        res.json({
+            msg: "homeAdded"
+        })
+    })
+    // res.json(req.body);
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var multer  = require('multer') // because urlencoded can't handle multiform stuff like files
+var upload = multer({ dest: './public/images' }) // hand multiform an object
+var db = require('../db');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+router.post('*', upload.single('locationImage'), (req, res, next)=>{
+	const token = req.body.token
+	const getUserIdQuery = `SELECT id FROM users WHERE token = ?`;
+    db.query(getUserIdQuery, [token], (err, results)=>{
+        if(results.length === 0){
+			res.locals.logginIn = false;
+        } else {
+			res.locals.logginIn = true;
+			res.locals.uid = results[0].id;
+			// res.locals.file = req.file;
+		}
+		next(); // send user on to next route
+    })
+})
 
 module.exports = router;
