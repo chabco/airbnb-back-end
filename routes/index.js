bb8067858d63c73b9dada3bfda2a2069bd09664b
charlescortez <charlesmph@gmail.com>
2019-09-26 15:11:57 -0400

    add cities route

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,37 @@
 var express = require('express');
 var router = express.Router();
-var multer  = require('multer') // because urlencoded can't handle multiform stuff like files
-var upload = multer({ dest: './public/images' }) // hand multiform an object
-var db = require('../db');
+var multer  = require('multer');
+var upload = multer({ dest: './public/images' });
+const db = require('../db');
 
 
-router.post('*', upload.single('locationImage'), (req, res, next)=>{
-	const token = req.body.token
-	const getUserIdQuery = `SELECT id FROM users WHERE token = ?`;
-    db.query(getUserIdQuery, [token], (err, results)=>{
+//check the user out to lock down our app!
+router.post('*',
+  upload.single('locationImage'),
+  (req, res, next)=>{
+    const token = req.body.token
+    const getUserIdQuery = `SELECT id FROM users WHERE token = ?`;
+    db.query(getUserIdQuery,[token], (err, results)=>{
         if(results.length === 0){
-			res.locals.logginIn = false;
-        } else {
-			res.locals.logginIn = true;
-			res.locals.uid = results[0].id;
-			// res.locals.file = req.file;
-		}
-		next(); // send user on to next route
+            res.locals.loggedIn = false;
+        }else{
+          res.locals.loggedIn = true;
+          res.locals.uid = results[0].id
+        }
+        next(); //send user on to next route
     })
 })
+
+
+router.get('/cities',(req, res, next)=>{
+  const citiesQuery = `SELECT * FROM cities
+    ORDER BY RAND()
+    LIMIT 8`
+  db.query(citiesQuery,(err, results)=>{
+    if(err) throw err;
+    res.json(results);
+  })
+})
+
 
 module.exports = router;
